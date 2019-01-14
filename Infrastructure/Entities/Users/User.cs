21a2b96d16184a0618bf931abeb819aab2ac17b5
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2019-01-14 21:54:12 +0545

    Test commit

--- Infrastructure/Entities/Users/User.cs
+++ Infrastructure/Entities/Users/User.cs
@@ -9,5 +9,7 @@ namespace Infrastructure.Entities.Users
     public class User
     {
         public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
