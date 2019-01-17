5e7fc730a975bc7775cbf085c60128e9584522de
red <skrishnau@gmail.com>
2019-01-17 21:38:44 +0545

    DTO renamed to ViewModel. "Core" folder in Service and ViewModel added to differentiate core and utility

---
+++ Infrastructure/Entities/Common/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities.Common
+{
+    public class Settings
+    {
+        public int Id { get; set; }
+        public int CurrentInvoiceNo { get; set; }
+
+    }
+}
---
+++ Service/Core/Users/Accounts/AccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Core.Users.Accounts
+{
+    public class AccountService:IAccountService
+    {
+    }
+}
