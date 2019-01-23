9ed55c02dd1ad2c5a96a0be93220e24da73db508
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2019-01-23 17:44:02 +0545

    Forms for supplier and customer

---
+++ IMS/Forms/Suppliers/Create/UC/AddSupplierUC.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace IMS.Forms.Suppliers.Create.UC
+{
+    partial class AddSupplierUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbContactPersonName = new System.Windows.Forms.MaskedTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbTelephone2 = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbError = new System.Windows.Forms.MaskedTextBox();
+            this.tbTelephone1 = new System.Windows.Forms.MaskedTextBox();
+            this.tbSupplierName = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbFax = new System.Windows.Forms.MaskedTextBox();
+            this.tbUserId = new System.Windows.Forms.MaskedTextBox();
+            this.tbWebsite = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // tbContactPersonName
+            // 
+            this.tbContactPersonName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbContactPersonName.Location = new System.Drawing.Point(242, 372);
+            this.tbContactPersonName.Name = "tbContactPersonName";
+            this.tbContactPersonName.Size = new System.Drawing.Size(203, 24);
+            this.tbContactPersonName.TabIndex = 47;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(48, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(188, 18);
+            this.label9.TabIndex = 46;
+            this.label9.Text = "Contact Person\'s Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(48, 339);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 18);
+            this.label8.TabIndex = 44;
+            this.label8.Text = "Website";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmail.Location = new System.Drawing.Point(200, 288);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(245, 24);
+            this.tbEmail.TabIndex = 43;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(48, 296);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 18);
+            this.label7.TabIndex = 42;
+            this.label7.Text = "Email";
+            // 
+            // tbTelephone2
+            // 
+            this.tbTelephone2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelephone2.Location = new System.Drawing.Point(200, 246);
+            this.tbTelephone2.Name = "tbTelephone2";
+            this.tbTelephone2.Size = new System.Drawing.Size(245, 24);
+            this.tbTelephone2.TabIndex = 41;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(48, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 18);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Telephone2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(48, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 18);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Fax";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 18);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "UserId";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(355, 414);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 43);
+            this.btnCancel.TabIndex = 35;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(250, 414);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(82, 43);
+            this.btnSave.TabIndex = 34;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tbError
+            // 
+            this.tbError.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbError.ForeColor = System.Drawing.Color.Red;
+            this.tbError.Location = new System.Drawing.Point(153, 475);
+            this.tbError.Name = "tbError";
+            this.tbError.Size = new System.Drawing.Size(292, 24);
+            this.tbError.TabIndex = 33;
+            // 
+            // tbTelephone1
+            // 
+            this.tbTelephone1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelephone1.Location = new System.Drawing.Point(200, 204);
+            this.tbTelephone1.Name = "tbTelephone1";
+            this.tbTelephone1.Size = new System.Drawing.Size(245, 24);
+            this.tbTelephone1.TabIndex = 32;
+            // 
+            // tbSupplierName
+            // 
+            this.tbSupplierName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSupplierName.Location = new System.Drawing.Point(200, 78);
+            this.tbSupplierName.Name = "tbSupplierName";
+            this.tbSupplierName.Size = new System.Drawing.Size(245, 24);
+            this.tbSupplierName.TabIndex = 31;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 18);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Telephone1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 18);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Supplier Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 20);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Add Supplier";
+            // 
+            // tbFax
+            // 
+            this.tbFax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFax.Location = new System.Drawing.Point(200, 162);
+            this.tbFax.Name = "tbFax";
+            this.tbFax.Size = new System.Drawing.Size(245, 24);
+            this.tbFax.TabIndex = 48;
+            // 
+            // tbUserId
+            // 
+            this.tbUserId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUserId.Location = new System.Drawing.Point(200, 120);
+            this.tbUserId.Name = "tbUserId";
+            this.tbUserId.Size = new System.Drawing.Size(245, 24);
+            this.tbUserId.TabIndex = 49;
+            // 
+            // tbWebsite
+            // 
+            this.tbWebsite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbWebsite.Location = new System.Drawing.Point(200, 330);
+            this.tbWebsite.Name = "tbWebsite";
+            this.tbWebsite.Size = new System.Drawing.Size(245, 24);
+            this.tbWebsite.TabIndex = 50;
+            // 
+            // AddSupplierUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbWebsite);
+            this.Controls.Add(this.tbUserId);
+            this.Controls.Add(this.tbFax);
+            this.Controls.Add(this.tbContactPersonName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbTelephone2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbError);
+            this.Controls.Add(this.tbTelephone1);
+            this.Controls.Add(this.tbSupplierName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Name = "AddSupplierUC";
+            this.Size = new System.Drawing.Size(493, 535);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox tbContactPersonName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox tbEmail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox tbTelephone2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.MaskedTextBox tbError;
+        private System.Windows.Forms.MaskedTextBox tbTelephone1;
+        private System.Windows.Forms.MaskedTextBox tbSupplierName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox tbFax;
+        private System.Windows.Forms.MaskedTextBox tbUserId;
+        private System.Windows.Forms.MaskedTextBox tbWebsite;
+    }
+}
