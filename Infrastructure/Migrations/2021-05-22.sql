59d69b4f408dfa38ae58eb5437d15559345170fb
skrishnau <skrishnau@gmail.com>
2021-06-12 22:54:11 +0545

    query text edited. but no change

--- Infrastructure/Migrations/2021-05-22.sql
+++ Infrastructure/Migrations/2021-05-22.sql
@@ -3,6 +3,4 @@ GO
 EXEC sp_rename 'dbo.PriceHistories.Price', 'Rate', 'COLUMN';  
 GO 
 -- 2021-06-12
-update AppSettings
-set Value = ''
-where Name = 'Password' or Name='Username'
+update AppSettings set Value = '' where Name = 'Password' or Name='Username'
